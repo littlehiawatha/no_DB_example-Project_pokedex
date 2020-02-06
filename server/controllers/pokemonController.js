0c0de054c8cbba41ec233924ae917f33293dd3b7
Jacob Hall <halljaco@yahoo.com>
2020-02-06 14:41:02 -0700

    Second  commit backEndFunctionalityWorking  Pokedex_Example 4.4.a no_DB_referenceTool

--- server/controllers/pokemonController.js
+++ server/controllers/pokemonController.js
@@ -3,12 +3,43 @@ const caughtPokemon = []
 let id = 0
 
 module.exports = {
-   getCaughtPokemon: (req, res) => {},
+   getCaughtPokemon: (req, res) => {
+      res.status(200).send(caughtPokemon)
+   },
 
-   catchPokemon: (req, res) => {},
+   catchPokemon: (req, res) => {
+      const {pokemon} = req.body
+      pokemon.id = id
+      id ++
+      caughtPokemon.push(pokemon)
 
-   editPokemonName: (req, res) => {},
+      res.status(200).send(caughtPokemon)
+   },
 
-   releasePokemon: (req, res) => {}
+   editPokemonName: (req, res) => {
+      const {id} = req.params
+      const {name} = req.body
+
+      const index = caughtPokemon.findIndex(element => {
+         return element.id === +id
+      })
+
+      caughtPokemon[index].name = name
+
+      res.status(200).send(caughtPokemon)
+   },
+
+   releasePokemon: (req, res) => {
+      const {id} = req.params
+
+      const index = caughtPokemon.findIndex(element => {
+         return element.id === +id
+      })
+
+      caughtPokemon.splice(index,1)
+      
+      res.status(200).send(caughtPokemon)
+
+   }
 
 }
--- server/index.js
+++ server/index.js
@@ -11,10 +11,10 @@ app.use(cors())
 
 app.get('/api/wild-pokemon', grassCtrl.getWildPokemon)
 
-app.get('/api/wild-pokemon', pokemonCtrl.getCaughtPokemon)
-app.post('/api/wild-pokemon', pokemonCtrl.catchPokemon)
-app.put('/api/wild-pokemon/:id', pokemonCtrl.editPokemonName)
-app.delete('/api/wild-pokemon/:id', pokemonCtrl.releasePokemon)
+app.get('/api/pokemon', pokemonCtrl.getCaughtPokemon)
+app.post('/api/pokemon', pokemonCtrl.catchPokemon)
+app.put('/api/pokemon/:id', pokemonCtrl.editPokemonName)
+app.delete('/api/pokemon/:id', pokemonCtrl.releasePokemon)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 // // I guarantee 80% of you will forget but you must do it or it will not WORK!!!!! package.json 
